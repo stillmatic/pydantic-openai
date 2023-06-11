624183a953bf42779d106acff02d35a9d531d2de
Chris Hua <hua.christopher@gmail.com>
2023-06-11 16:45:20 -0700

    chore: update chat

--- src/pydantic_openai/__init__.py
+++ src/pydantic_openai/__init__.py
@@ -3,3 +3,5 @@ from .common import *
 from .completion import *
 from .embeddings import *
 from .moderation import *
+
+__version__ = "0.1.0"
--- src/pydantic_openai/chat.py
+++ src/pydantic_openai/chat.py
@@ -4,8 +4,9 @@ Reference: https://platform.openai.com/docs/api-reference/completions
 """
 from typing import List, Dict, Optional
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from .common import Usage
+
 
 class ChatMessageRole(str, Enum):
     System = "system"
@@ -15,32 +16,31 @@ class ChatMessageRole(str, Enum):
 class ChatCompletionMessage(BaseModel):
     role: ChatMessageRole
     content: str
-    name: Optional[str] = None
+    name: Optional[str] = Field(None, alias="name")
 
 class ChatCompletionRequest(BaseModel):
     model: str
     messages: List[ChatCompletionMessage]
-    max_tokens: Optional[int] = None
-    temperature: Optional[float] = None
-    top_p: Optional[float] = None
-    n: Optional[int] = None
-    stream: bool = False
-    stop: Optional[List[str]] = None
-    presence_penalty: Optional[float] = None
-    frequency_penalty: Optional[float] = None
-    logit_bias: Optional[Dict[str, int]] = None
-    user: Optional[str] = None
+    max_tokens: Optional[int] = Field(None, alias="max_tokens")
+    temperature: Optional[float] = Field(None, alias="temperature")
+    top_p: Optional[float] = Field(None, alias="top_p")
+    n: Optional[int] = Field(None, alias="n")
+    stream: Optional[bool] = Field(None, alias="stream")
+    stop: Optional[List[str]] = Field(None, alias="stop")
+    presence_penalty: Optional[float] = Field(None, alias="presence_penalty")
+    frequency_penalty: Optional[float] = Field(None, alias="frequency_penalty")
+    logit_bias: Optional[Dict[str, int]] = Field(None, alias="logit_bias")
+    user: Optional[str] = Field(None, alias="user")
 
 class ChatCompletionChoice(BaseModel):
     index: int
     message: ChatCompletionMessage
-    finish_reason: str
+    finish_reason: str = Field(..., alias="finish_reason")
 
 class ChatCompletionResponse(BaseModel):
-    id: str
-    object: str
-    created: int
-    model: str
+    id: str = Field(..., alias="id")
+    object: str = Field(..., alias="object")
+    created: int = Field(..., alias="created")
+    model: str = Field(..., alias="model")
     choices: List[ChatCompletionChoice]
     usage: Usage
-
